16224ee45e639432b28bc2311067dde9a862630d
josed <josedegracia1212@gmail.com>
2024-06-01 16:55:40 -0500

    vinculacion de html con api rest, done

--- src/main/java/dev/josedegracia/jamboardReplacement/CorsConfig.java
+++ src/main/java/dev/josedegracia/jamboardReplacement/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://tmpjoe.github.io")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 }
